2c7ee0dcd04fced17483f99d4cb6570b2f2250ac
Jack Keller <jwallkeller@gmail.com>
2022-10-10 17:46:50 -0400

    dbService created

--- client/index.js
+++ client/index.js
@@ -1,6 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    loadHTMLTable([]);
+    fetch('http://localhost:5000/getAll')
+    .then(response => response.json())
+    .then(data => loadHTMLTable(data['data']));
+    
 });
+
+const addBtn = document.querySelector('#add-name-btn');
+
+addBtn.onclick = function () {
+    const nameInput = document.querySelector('#name-input');
+    const name = nameInput.value;
+    nameInput.value = "";
+    
+    fetch('http://localhost:5000/insert', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({ name : name })
+    })
+    .then(response => response.json())
+    .then(data => insertRowIntoTable(data['data']));
+}
+
+function insertRowIntoTable(data) {
+
+}
 
 function loadHTMLTable(data) {
     const table = document.querySelector('table tbody');
--- server/app.js
+++ server/app.js
@@ -4,18 +4,34 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const dbService = require('./dbService')
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : false }));
 
 //create
 app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ success: true }))
+    .catch(err => console.log(err));
 
 });
 
 //read
 app.get('/getAll', (request, response) => {
-    console.log('test');
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data: data}))
+    .catch(err => console.log(err));
 });
 
 //update
